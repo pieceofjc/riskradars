fefe24e3d8984655b520aa57607130766b3ef817
username <user@email.com>
2025-07-07 13:23:52 +0900

    real result list

--- migration.py
+++ migration.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 # CSV 파일 읽기
-df = pd.read_csv("corp_list2.csv", dtype={'종목코드':str}, encoding="utf-8")
+df = pd.read_csv("corp_result_list.csv", dtype={'stock_code':str})
 
 # SQLite DB 연결 (없으면 자동 생성됨)
 conn = sqlite3.connect("rrdb.db")
@@ -12,4 +12,5 @@ df.to_sql("corp_info", conn, if_exists="replace", index=False)
 
 df_check = pd.read_sql("SELECT * FROM corp_info LIMIT 5", conn)
 print(df_check)
+
 conn.close()
